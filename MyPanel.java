2e242fc37d7c7c4e7c13f38157a01984c8f92010
John Brown <100054328@csisd.org>
2024-08-21 20:47:11 -0500

    added the check method

--- MyPanel.java
+++ MyPanel.java
@@ -5,7 +5,7 @@ import javax.swing.*;
 public class MyPanel extends JPanel implements ActionListener {
 
 Timer timer;
-Boolean[][] tiles;
+int[][] tiles;
 int tileWidth;
 int tileHeight;
  
@@ -17,10 +17,10 @@ int tileHeight;
   this.setPreferredSize(new Dimension(startWidth,startHeight));
   tileWidth = startWidth/10;
   tileHeight = startHeight/10;
-  tiles = new Boolean[tileWidth][tileHeight];
+  tiles = new int[tileWidth][tileHeight];
   //(int)(Math.random() * tileWidth) + 1//This is the random code for width I guess
   for (int i = 0; i < 50; i++) {
-    tiles[(int)(Math.random() * tileWidth) + 0][(int)(Math.random() * tileHeight) + 0] = true;
+    tiles[(int)(Math.random() * tileWidth) + 0][(int)(Math.random() * tileHeight) + 0] = 1;
   }
   timer = new Timer(5, this);
 	timer.start();
@@ -37,11 +37,36 @@ int tileHeight;
 
   g2D.setPaint(Color.white);
 
+  for (int x = 0; x < tileWidth; x++) {
+    for (int y = 0; y < tileHeight; y++) {
+      if (tiles[x][y] == 1) {
+        g2D.fillRect((x) * 10, (y) * 10, 10, 10);
+      }
+    }
+  }
+
   g2D.setFont(new Font("Comic Sans MS",Font.BOLD,20));
   
  }
   @Override
 	public void actionPerformed(ActionEvent e) {
     repaint();
+  }
+
+  public int surroundCount(int xTile, int yTile) {
+    int total = 0;
+    if ((xTile != 0 && xTile != tileWidth) && (yTile != 1 && yTile != tileHeight)) {
+      //Bottom Row
+      total += tiles[xTile - 1][yTile - 1];
+      total += tiles[xTile][yTile - 1];
+      total += tiles[xTile + 1][yTile - 1];
+      //Top Row
+      total += tiles[xTile - 1][yTile + 1];
+      total += tiles[xTile][yTile + 1];
+      total += tiles[xTile + 1][yTile + 1];
+      //Middle
+      total += tiles[xTile - 1][yTile];
+      total += tiles[xTile + 1][yTile];
+    }
   }
 }
